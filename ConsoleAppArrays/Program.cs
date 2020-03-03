a43f863f5a9366601aae1eff25a63d3c3263e5d5
Илья <vazerlan@gmail.com>
2020-03-03 12:23:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppArrays/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleAppArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = Enumerable.Range(3, 4).ToArray();
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(myArray[i]);
+            }
+        }
+    }
+}
---
+++ Write_In_Keyboard/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Write_In_Keyboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Укажите количество элементов для массива");
+            int count = int.Parse(Console.ReadLine());
+
+            int[] myArray = new int[count];
+
+            Console.WriteLine("Заполните массив: ");
+            for (int i = 0; i < count; i++)
+            {
+                myArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.Write("\nМассив myArray = { ");
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(myArray[i]);
+                if (i < count - 1)
+                    Console.Write(", ");
+            }
+            Console.WriteLine(" }");
+
+        }
+    }
+}
